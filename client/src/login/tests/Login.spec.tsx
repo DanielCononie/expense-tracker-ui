3d4b6983361d0baa61cfa0ca480ad7bfae278f49
Daniel Cononie <danielcononie@ca4209b6-146e-4926-85a5-752ac9efdd1a.attlocal.net>
2025-08-31 23:31:13 -0400

    added additional test

--- client/src/login/tests/Login.spec.tsx
+++ client/src/login/tests/Login.spec.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Login } from "../Login";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("Login Page", () => {
   it("should render login page", async () => {
@@ -22,5 +23,28 @@ describe("Login Page", () => {
     expect(password).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
     expect(createAccountButton).toBeInTheDocument();
+  });
+
+  it("should fire error message when login button is clicked", async () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>,
+    );
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByRole("button", { name: /login/i }));
+
+    const err = await screen.findByText(/username and password are required/i);
+    expect(err).toBeInTheDocument();
+
+    await user.type(screen.getByLabelText(/username/i), "daniel");
+    await user.type(screen.getByLabelText(/password/i), "secret");
+    await user.click(screen.getByRole("button", { name: /login/i }));
+
+    expect(
+      screen.queryByText(/username and password are required/i),
+    ).not.toBeInTheDocument();
   });
 });
